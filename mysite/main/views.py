14254f3ff8ccb7603af8302766a69421a3cb3979
kannoyeskanno <jomarasisgriffin@gmail.com>
2022-11-14 18:01:30 +0800

    try

---
+++ mysite/main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def index(response):
+	return render(response,  "main/base.html", {})
+
+def signup(response):
+	return render(response, "main/signup.html", {})
+
+
+def login(response):
+	return render(response, "main/login.html", {})
+
+#
+# def index(response):
+# 	return HttpResponse("<h1>aaaaa</h1>")
+# def signUp(response):
+# 	return HttpResponse("<h1>signusadasd</h1>")
+#
+#
+#
