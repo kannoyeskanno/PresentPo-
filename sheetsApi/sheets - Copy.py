4d969e8299ef765d3602de70e577e31ae273fe91
kannoyeskanno <jomarasisgriffin@gmail.com>
2022-11-28 10:07:59 +0800

    Cam/sheets

---
+++ sheetsApi/sheets - Copy.py
@@ -0,0 +1,31 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import date
+import time
+from main import DemoApp as a
+
+scope = [
+'https://www.googleapis.com/auth/spreadsheets',
+'https://www.googleapis.com/auth/drive'
+]
+creds = ServiceAccountCredentials.from_json_keyfile_name('client.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open('Attendance').sheet1
+
+name = a.qr_value
+def checkDate():
+    cell = sheet.find(date.today().strftime('%B %d %Y'))
+    if cell is not None:
+        return cell.col
+
+def checkName():
+    cell = sheet.find(name)
+    if cell is not None:
+        return cell.row
+
+def updateSheet():
+    sheet.update_cell(checkName(), checkDate(), time.strftime("%I : %M %p"))
+
+
+updateSheet()
+
